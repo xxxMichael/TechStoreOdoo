b0b2c2d7ad2b9e16431dcbbb6e72d4bf2f8638e7
xxxmichael <mayckolchavez27@gmail.com>
2026-06-02 20:22:21 -0500

    add: tests

---
+++ custom_addons/techstore_maintenance/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from . import test_maintenance_order
+from . import test_technician
+from . import test_equipment
+from . import test_audit_log
+from . import test_integration
+from . import test_api_controller
